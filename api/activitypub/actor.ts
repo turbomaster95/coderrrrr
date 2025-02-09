1dd576adc13c3a0501123c7230c69bd79900b0a4
Deva Midhun <admin@coderrrrr.site>
2025-02-09 17:08:38 +0000

    update actor

--- api/activitypub/actor.ts
+++ api/activitypub/actor.ts
@@ -18,7 +18,7 @@ export default async function (req: VercelRequest, res: VercelResponse) {
     "sharedInbox": "https://coderrrrr.site/api/activitypub/sharedInbox",
     "inbox": "https://coderrrrr.site/api/activitypub/inbox",
     "url": "https://coderrrrr.site/blog"
-    "published": "2025-02-09T17:01:02Z",
+    "published": "2024-04-06T17:01:02Z",
     "preferredUsername": "blog",
     "name": "Deva Midhun's blog",
     "manuallyApprovesFollowers": false,
